380585d065f6dc624b02852626e76e02931427b9
Connor McCue <connor.mccue@comcast.net>
2022-09-30 16:50:21 -0700

    progress on Todo.js but unfinished, wanted to commit

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
+import { useTodos } from '../../hooks/useTodos';
+import { toggleTodoItem, createTodoItem } from '../../services/items';
 import './Todo.css';
 
 export default function Todo() {
-  const id = useParams();
-  console.log('todo');
+  const [description, setDescription] = useState('');
+  const { todos, setTodos } = useTodos();
+
+  const { user } = useContext(UserContext);
+  if (!user) {
+    return <Redirect to="/auth/sign-in" />;
+  }
+  const handleClick = async (todo) => {
+    try {
+      const updatedTodo = await toggleTodoItem(todo);
+      setTodos((prevTodos) =>
+        prevTodos.map((prevTodo) => (prevTodo.id === todo.id ? updatedTodo: prevTodo))
+      );
+    } catch (e) {
+      //eslint-disable-next-line no-console
+      console.error(e.message);
+    }
+  };
+
+  const handleNewTodo = async () => {
+    try {
+      await createTodoItem(description);
+      setTodos((prev) => [...prev, (description)]);
+      setName('');
+    } catch (e) {
+      //eslint-disable-next-line no-console
+      console.error(e.message);
+    }
+  };
+
+
   return (
     <main>
-      <div>todo1</div>
-      <div>todo2</div>
-      <div>todo3</div>
+      <div>
+        {todos.map((todo) => ( 
+          <div key={todo.id}> 
+            <label className="checkbox">
+              <input
+                className="m-1"
+                type="checkbox"
+                checked={todo.complete}
+                onClick={() => handleClick(todo)}
+              />
+              {todo.description}
+            </label>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
